70336c77393992e060492500223cb46fb570500b
Mathematic1 <NightRaven19950528@gmail.com>
2025-01-29 22:30:19 +0300

    fixed namespace for IResource

--- RHICommon/Common/Resources.hpp
+++ RHICommon/Common/Resources.hpp
@@ -1,20 +1,24 @@
 #pragma once
 #include <memory>
 
-class IResource
+namespace RHI
 {
-public:
-//TODO: fix access memory
-//protected:
-	IResource() = default;
-	virtual ~IResource() = default;
+    class IResource
+    {
+    public:
+        //TODO: fix access memory
+        //protected:
+        IResource() = default;
+        virtual ~IResource() = default;
 
-public:
-    // Non-copyable and non-movable
-    IResource(const IResource&) = delete;
-    IResource(const IResource&&) = delete;
-    IResource& operator=(const IResource&) = delete;
-    IResource& operator=(const IResource&&) = delete;
-};
+    public:
+        // Non-copyable and non-movable
+        IResource(const IResource&) = delete;
+        IResource(const IResource&&) = delete;
+        IResource& operator=(const IResource&) = delete;
+        IResource& operator=(const IResource&&) = delete;
+    };
 
-typedef std::shared_ptr<IResource> ResourceHandle;
+    typedef std::shared_ptr<IResource> ResourceHandle;
+
+}
