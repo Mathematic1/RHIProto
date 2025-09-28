0316d5668c27feedf9a9003d8a2193e4625bc7e3
Aleksey Komarov <q4arus@ya.ru>
2025-09-28 10:35:57 +0300

    add Linux support

--- RHIVulkan/VulkanBackend.hpp
+++ RHIVulkan/VulkanBackend.hpp
@@ -7,6 +7,7 @@
 
 #include <map>
 #include <array>
+#include <list>
 #include <memory>
 #include <mutex>
 #include <string>
--- RHIVulkan/VulkanBuffer.cpp
+++ RHIVulkan/VulkanBuffer.cpp
@@ -1,4 +1,5 @@
 #include <VulkanBackend.hpp>
+#include <cstring>
 
 //#include <EasyProfilerWrapper.hpp>
 
